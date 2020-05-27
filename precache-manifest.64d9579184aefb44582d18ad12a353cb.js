d838a7c64c3d24a0ea3122a9e7b199a6e694e11c
Khoi Dao <48459899+Khoidao55@users.noreply.github.com>
2020-05-26 22:17:46 -0700

    Updates

---
+++ precache-manifest.64d9579184aefb44582d18ad12a353cb.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f915eaa0ce13e36a5e4b201c7af6f5d8",
+    "url": "/spotify/index.html"
+  },
+  {
+    "revision": "6464395b8ab7669ebf9c",
+    "url": "/spotify/static/css/main.8381f14b.chunk.css"
+  },
+  {
+    "revision": "ddb2c52f43b2f494215f",
+    "url": "/spotify/static/js/2.484cfce1.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/spotify/static/js/2.484cfce1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6464395b8ab7669ebf9c",
+    "url": "/spotify/static/js/main.7b582ff9.chunk.js"
+  },
+  {
+    "revision": "a2ef6f928ed4ec1d8c13",
+    "url": "/spotify/static/js/runtime-main.9572b5c8.js"
+  },
+  {
+    "revision": "c54e376db2831ac5bf68f3597d3686ac",
+    "url": "/spotify/static/media/1.c54e376d.jpg"
+  },
+  {
+    "revision": "e45b0e822cbdbce968c60df0ba2421d5",
+    "url": "/spotify/static/media/2.e45b0e82.jpg"
+  },
+  {
+    "revision": "c141afac987773ae75379628635b35f7",
+    "url": "/spotify/static/media/3.c141afac.jpg"
+  },
+  {
+    "revision": "37c443aed30ea5abc7c0ac41814724a4",
+    "url": "/spotify/static/media/4.37c443ae.jpg"
+  },
+  {
+    "revision": "1ef80642cbf5e264e30d9c812ca72e72",
+    "url": "/spotify/static/media/5.1ef80642.jpg"
+  },
+  {
+    "revision": "2fed152f44ff548535643dce57d89bd9",
+    "url": "/spotify/static/media/6.2fed152f.jpg"
+  },
+  {
+    "revision": "497b6e67346ac3d231770c22fe5038ab",
+    "url": "/spotify/static/media/8.497b6e67.jpg"
+  },
+  {
+    "revision": "115e389dce9cb979a545e1f3b3d86cf3",
+    "url": "/spotify/static/media/Bao-image.115e389d.png"
+  },
+  {
+    "revision": "b3e0b571edea169cb471ebde3568517e",
+    "url": "/spotify/static/media/Chainsmokers_Roses.b3e0b571.mp3"
+  },
+  {
+    "revision": "da7c3ac82687e957fcf1d45f99be25a6",
+    "url": "/spotify/static/media/Darian-image.da7c3ac8.png"
+  },
+  {
+    "revision": "05546fbc7c60117520587ff89019c745",
+    "url": "/spotify/static/media/David-image.05546fbc.png"
+  },
+  {
+    "revision": "52e9129f2d4484af67b441fc37281960",
+    "url": "/spotify/static/media/Elephante.52e9129f.mp3"
+  },
+  {
+    "revision": "d593fdc65d4156eb3ed03302527fc66b",
+    "url": "/spotify/static/media/Gryffin_TieMeDown.d593fdc6.mp3"
+  },
+  {
+    "revision": "7a78fc340bcfb58248d92e03d26d983a",
+    "url": "/spotify/static/media/Illenium_CrawlOutOfLove.7a78fc34.mp3"
+  },
+  {
+    "revision": "ce0b113076ebca936c54da7799d821f7",
+    "url": "/spotify/static/media/Marshmello_Alone.ce0b1130.mp3"
+  },
+  {
+    "revision": "63a04a41e701e6cf612fc78d68dc5b2d",
+    "url": "/spotify/static/media/Ngoc-image.63a04a41.png"
+  },
+  {
+    "revision": "3fc15391ff115d01ad312c569df38e41",
+    "url": "/spotify/static/media/SanHolo_Light.3fc15391.mp3"
+  },
+  {
+    "revision": "f104eae9d94b2fde4505d924f1f7a03f",
+    "url": "/spotify/static/media/Slander.f104eae9.mp3"
+  },
+  {
+    "revision": "ee83dc03ca17686e19a9cba0db8519fa",
+    "url": "/spotify/static/media/Zedd_Clarity.ee83dc03.mp3"
+  },
+  {
+    "revision": "10bc4bba7e76f5959b2815e628535334",
+    "url": "/spotify/static/media/android-btn.10bc4bba.png"
+  },
+  {
+    "revision": "7328ca53e4f6b104eae0a34072e6dcf8",
+    "url": "/spotify/static/media/apple-btn.7328ca53.png"
+  },
+  {
+    "revision": "70c394e45a6e6f0670e053b832cdb19f",
+    "url": "/spotify/static/media/background.70c394e4.jpg"
+  },
+  {
+    "revision": "c39a19ec931c35f5a62e6515566a916d",
+    "url": "/spotify/static/media/background.c39a19ec.png"
+  },
+  {
+    "revision": "99539b50354b17e2e3660204bd455fcc",
+    "url": "/spotify/static/media/coachella-test.99539b50.png"
+  },
+  {
+    "revision": "3d811ad2186a99a2435e4a97fafc4fef",
+    "url": "/spotify/static/media/countdown.3d811ad2.png"
+  },
+  {
+    "revision": "18b3a9b204b9dec85332c55a71bc974c",
+    "url": "/spotify/static/media/elephante.18b3a9b2.png"
+  },
+  {
+    "revision": "95388af9390f27a46d7aeb88ce00f3d6",
+    "url": "/spotify/static/media/iphone-spotify.95388af9.png"
+  },
+  {
+    "revision": "96a319e006c8f619e4e6f1c5e03e731f",
+    "url": "/spotify/static/media/logo.96a319e0.png"
+  },
+  {
+    "revision": "f5d36e3e74a9f22567dcb727e1527ad5",
+    "url": "/spotify/static/media/tomorrowland.f5d36e3e.png"
+  },
+  {
+    "revision": "95907668ded63c425a5f0b90e9aa9de4",
+    "url": "/spotify/static/media/user.95907668.png"
+  }
+]);
